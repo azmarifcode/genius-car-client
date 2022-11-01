f2dbdfcd9fac92765bb531ac1ebc7804a41e4c96
A. Z. M. Arif <azmarifcode@gmail.com>
2022-11-02 01:29:30 +0600

    first commit

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: 'AIzaSyAC7w3umG7Bpo8Swx33G2bztnKtJpXImLQ',
+    authDomain: 'genius-car-378da.firebaseapp.com',
+    projectId: 'genius-car-378da',
+    storageBucket: 'genius-car-378da.appspot.com',
+    messagingSenderId: '83037525786',
+    appId: '1:83037525786:web:0ba4c62df875f17c73bea3',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
